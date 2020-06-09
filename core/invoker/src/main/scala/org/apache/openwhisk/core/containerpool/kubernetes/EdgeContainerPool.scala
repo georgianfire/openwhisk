d00ff70d57eda9c50a29e706eb1e516308a4028e
Bin Wang <jerry.binwang@gmail.com>
2020-06-08 20:44:04 -0400

    Add EdgeContainerPool

---
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/kubernetes/EdgeContainerPool.scala
@@ -0,0 +1,96 @@
+package org.apache.openwhisk.core.containerpool.kubernetes
+
+import akka.actor.{Actor, ActorRef, ActorRefFactory, Props}
+import org.apache.openwhisk.common.{AkkaLogging, Logging, LoggingMarkers, MetricEmitter}
+import org.apache.openwhisk.core.connector.ActivationMessage
+import org.apache.openwhisk.core.containerpool.{ContainerId, ContainerRemoved, Resize, Run, RunWithSize}
+import org.apache.openwhisk.core.entity.{ByteSize, CpuTime, ExecutableWhiskAction}
+import org.apache.openwhisk.core.entity.size._
+
+import scala.collection.immutable
+import scala.concurrent.ExecutionContext
+
+sealed trait EdgeContainerOperationMessage
+
+object EdgeContainerOperationMessage {
+  case class CreateContainer() extends EdgeContainerOperationMessage
+
+  case class DeleteContainer() extends EdgeContainerOperationMessage
+
+  case class ResizeContainer() extends EdgeContainerOperationMessage
+
+  case class GracefulTerminateContainer() extends EdgeContainerOperationMessage
+}
+
+sealed trait ActionExecutionMessage
+
+case class EdgeContainerData(isEphermal: Boolean)
+
+object ActionExecutionMessage {
+  case class RunWithEphemeralContainer(action: ExecutableWhiskAction,
+                                       msg: ActivationMessage) extends ActionExecutionMessage
+
+  case class RunOnContainer(action: ExecutableWhiskAction,
+                            msg: ActivationMessage,
+                            containerId: ContainerId) extends ActionExecutionMessage
+}
+
+class EdgeContainerPool(containerFactory: ActorRefFactory => ActorRef) extends Actor {
+  implicit val logging: Logging = new AkkaLogging(context.system.log)
+  implicit val executionContext: ExecutionContext = context.dispatcher
+
+  var pool = immutable.Map.empty[ContainerId, ActorRef]
+  var containerDataByRef = immutable.Map.empty[ActorRef, EdgeContainerData]
+
+  override def receive: Receive = {
+    case op: EdgeContainerOperationMessage => {
+      // Message Sent from resource manager
+      import EdgeContainerOperationMessage._
+
+      op match {
+        case CreateContainer() => ???
+        case DeleteContainer() => ???
+        case ResizeContainer() => ???
+        case GracefulTerminateContainer() => ???
+      }
+    }
+
+    case msg: ActionExecutionMessage => {
+      // Message sent from loadbalancer
+      import ActionExecutionMessage._
+
+      msg match{
+        case RunWithEphemeralContainer(action, msg) =>
+          val ephemeralContainer = containerFactory(context)
+          ephemeralContainer ! RunWithSize(action, msg, 256.MB, CpuTime(200))
+          ephemeralContainer ! Resize(None, Some(CpuTime(100)))
+          containerDataByRef = containerDataByRef.updated(ephemeralContainer, EdgeContainerData(isEphermal=true))
+
+        case RunOnContainer(action, msg, containerId) =>
+          assert(pool.contains(containerId))
+          val container = pool(containerId)
+          container ! Run(action, msg)
+      }
+    }
+
+    case ContainerRemoved =>
+      val containerData = containerDataByRef(sender())
+      if (containerData.isEphermal) {
+        logging.info(this, "Emphermal container removed.")
+        containerDataByRef = containerDataByRef - sender()
+      }
+      else ???
+
+    case unknownMessage => logging.error(this, s"Received unknown message: $unknownMessage")
+  }
+
+  def createContainer(memoryLimit: ByteSize, cpuTime: CpuTime) = ???
+
+  private def emitMetrics(): Unit = {
+    MetricEmitter.emitGaugeMetric(LoggingMarkers.CONTAINER_POOL_ACTIVE_COUNT, pool.size)
+  }
+}
+
+object EdgeContainerPool {
+  def props(factory: ActorRefFactory => ActorRef) = Props(new EdgeContainerPool(factory))
+}
